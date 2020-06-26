dec5399225d9ba4f51103f297ede68dc7fdcf573
miriamschaefer <msuarezmelian@gmail.com>
2020-06-26 19:38:37 +0200

    add first js functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const print = document.querySelector(".js-smiley");
+const clickUpdate = document.querySelector(".js-button");
+const inputSelector = document.querySelector(".js-selector");
+
+function changeSmiley(event) {
+  print.innerHTML = inputSelector.value;
+}
+
+clickUpdate.addEventListener("click", changeSmiley);
