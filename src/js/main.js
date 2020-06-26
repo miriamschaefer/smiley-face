3bf1063bde24f88045646dfb3078223c8145e1ce
miriamschaefer <msuarezmelian@gmail.com>
2020-06-26 20:42:45 +0200

    finished product

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,22 @@
 const print = document.querySelector(".js-smiley");
 const clickUpdate = document.querySelector(".js-button");
 const inputSelector = document.querySelector(".js-selector");
+const background = document.querySelector(".main");
 
-function changeSmiley(event) {
+function changeSmiley() {
   print.innerHTML = inputSelector.value;
+  console.log(changeBackground(0, 100));
 }
 
-clickUpdate.addEventListener("click", changeSmiley);
+function changeBackground(min, max) {
+  const randomNumber = Math.round(Math.random() * (max - min) + min);
+  console.log(randomNumber);
+
+  if (randomNumber % 2 === 0) {
+    return background.classList.add("background2");
+  } else {
+    return background.classList.remove("background2");
+  }
+}
+
+clickUpdate.addEventListener("click", changeSmiley, changeBackground);
