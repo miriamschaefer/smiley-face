56e547bac30f975def6035931850235ce273c922
miriamschaefer <msuarezmelian@gmail.com>
2020-06-29 16:01:15 +0200

    code correction

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,28 @@
-"use strict";
+'use strict';
 
-const print = document.querySelector(".js-smiley");
-const clickUpdate = document.querySelector(".js-button");
-const inputSelector = document.querySelector(".js-selector");
-const background = document.querySelector(".main");
+const print = document.querySelector('.js-smiley');
+const clickUpdate = document.querySelector('.js-button');
+const inputSelector = document.querySelector('.js-selector');
+const background = document.querySelector('.main');
 
 function changeSmiley() {
   print.innerHTML = inputSelector.value;
-  console.log(changeBackground(0, 100));
 }
 
-function changeBackground(min, max) {
-  const randomNumber = Math.round(Math.random() * (max - min) + min);
+function changeBackground() {
+  const randomNumber = parseInt(Math.random() * 100);
   console.log(randomNumber);
 
   if (randomNumber % 2 === 0) {
-    return background.classList.add("background2");
+    return background.classList.add('background2');
   } else {
-    return background.classList.remove("background2");
+    return background.classList.remove('background2');
   }
 }
 
-clickUpdate.addEventListener("click", changeSmiley, changeBackground);
+function submitHandler() {
+  changeSmiley();
+  changeBackground();
+}
+
+clickUpdate.addEventListener('click', submitHandler);
